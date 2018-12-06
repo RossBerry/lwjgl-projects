07be3bb0a57475e0e4200afd39f4a651fe8d33a6
Kenneth Berry <kberry14@msudenver.edu>
2018-12-06 01:23:15 -0700

    Update comments for Simple-Car/Ex9.java

--- Simple-Car/Ex9.java
+++ Simple-Car/Ex9.java
@@ -1,307 +1,287 @@
-/*  
-  Ex9
+/**  
+ * Ex9
+ *
+ * @author Kenneth Berry
 */
 
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
-
-// import static org.lwjgl.glfw.Callbacks.*;
- import static org.lwjgl.glfw.GLFW.*;   // just for the key constants
-// import static org.lwjgl.system.MemoryUtil.*;
-
+//import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*; // just for the key constants
+//import static org.lwjgl.system.MemoryUtil.*;
 import java.util.Scanner;
 import java.io.*;
 import java.nio.FloatBuffer;
-
 import java.util.ArrayList;
 
-public class Ex9 extends Basic {
+public class Ex9 extends Basic 
+{
 
-   public static void main(String[] args) {
-      Ex9 app = new Ex9( "Exercise 9", 1000, 500, 30, args[0] );
-      app.start();
-   }// main
+  /**
+   * main (overrides Basic.main())
+   */
+  public static void main(String[] args)
+  {
+    Ex9 app = new Ex9( "Exercise 9", 1000, 500, 30, args[0] );
+    app.start();
+  } // main
 
-   // instance variables 
+  /**
+    * Instance variables
+    */
+  private Shader v1, f1;
+  private int hp1;  // handle for the GLSL program
+  // have two matrices in shader---projection and viewing
+  private int projLoc, viewLoc;
+  // for first person view, use frustum and lookAt
+  private FloatBuffer frustumBuffer, lookAtBuffer;
+  // for map view, use ortho and mapView
+  private FloatBuffer orthoBuffer, mapBuffer;
+  private ArrayList<Thing> things;
+  private TriList tris;
+  private int texture1Loc;
+  private Camera camera;
  
-   private Shader v1, f1;
-   private int hp1;  // handle for the GLSL program
- 
-   // have two matrices in shader---projection and viewing
-   private int projLoc, viewLoc;
-     // for first person view, use frustum and lookAt
-     private FloatBuffer frustumBuffer, lookAtBuffer;
-     // for map view, use ortho and mapView
-     private FloatBuffer orthoBuffer, mapBuffer;
- 
-   private ArrayList<Thing> things;
 
-   private TriList tris;
+  // construct basic application with given title, pixel width and height
+  // of drawing area, and frames per second
+  public Ex9( String appTitle, int pw, int ph, int fps, String inputFile )
+  {
+    super( appTitle, pw, ph, (long) ((1.0/fps)*1000000000) );
  
-   private int texture1Loc;
-   
-   private Camera camera;
- 
-   // construct basic application with given title, pixel width and height
-   // of drawing area, and frames per second
-   public Ex9( String appTitle, int pw, int ph, int fps, String inputFile )
-   {
-     super( appTitle, pw, ph, (long) ((1.0/fps)*1000000000) );
- 
-     // load all the textures
-     Pic.init();
+    // load all the textures
+    Pic.init();
 
-     // read data to build all the Things
-     try {
-        Scanner input = new Scanner( new File( inputFile ) );
-        int number = input.nextInt();  input.nextLine();
-
-        things = new ArrayList<Thing>();
-
-        for (int k=0; k<number; k++) {
-           things.add( new Thing( input ) );           
-        }
-
-        input.close();
-     }
-     catch(Exception e) {
-        e.printStackTrace();
-        System.exit(1);
-     }
- 
-     // place camera somewhere at start
-     camera = new Camera( -.1, .1, -.1, .1, 0.5, 300,
-                           new Triple(50,50,30), 90, -30 ); 
-   }
- 
-   protected void init()
-   {
-     String vertexShaderCode =
- "#version 330 core\n"+
- "layout (location = 0 ) in vec3 vertexPosition;\n"+
- "layout (location = 1 ) in vec2 vertexTexCoord;\n"+
- "out vec2 texCoord;\n"+
- "uniform mat4 projection;\n"+
- "uniform mat4 view;\n"+
- "void main(void)\n"+
- "{\n"+
- "  texCoord = vertexTexCoord;\n"+
- "  gl_Position = projection * view * vec4( vertexPosition, 1.0 );\n"+
- "}\n";
- 
-     System.out.println("Vertex shader:\n" + vertexShaderCode + "\n\n" );
- 
-     v1 = new Shader( "vertex", vertexShaderCode );
- 
-     String fragmentShaderCode =
- "#version 330 core\n"+
- "in vec2 texCoord;\n"+
- "layout (location = 0 ) out vec4 fragColor;\n"+
- "uniform sampler2D texture1;\n"+
- "void main(void)\n"+
- "{\n"+
- "  fragColor = texture( texture1, texCoord );\n"+
- "}\n";
- 
-     System.out.println("Fragment shader:\n" + fragmentShaderCode + "\n\n" );
- 
-     f1 = new Shader( "fragment", fragmentShaderCode );
- 
-     hp1 = GL20.glCreateProgram();
-          Util.error("after create program");
-          System.out.println("program handle is " + hp1 );
- 
-     GL20.glAttachShader( hp1, v1.getHandle() );
-          Util.error("after attach vertex shader to program");
- 
-     GL20.glAttachShader( hp1, f1.getHandle() );
-          Util.error("after attach fragment shader to program");
- 
-     GL20.glLinkProgram( hp1 );
-          Util.error("after link program" );
- 
-     GL20.glUseProgram( hp1 );
-          Util.error("after use program");
- 
-     // get location of uniforms 
-     projLoc = GL20.glGetUniformLocation( hp1, "projection" );
-     viewLoc = GL20.glGetUniformLocation( hp1, "view" );
-     texture1Loc = GL20.glGetUniformLocation( hp1, "texture1" );
- 
-     // once and for all (it's never changing), get
-     // buffer for frustum
-     frustumBuffer = camera.getFrustumBuffer();
-     // and create the fixed ortho and identity matrix buffers
-     orthoBuffer = Util.createFloatBuffer( 16 );
-     Mat4 ortho = Mat4.ortho( -50, 50, -50, 50, 0, 101 );
-System.out.println("ortho: \n" + ortho );
-     ortho.sendData( orthoBuffer );
-     mapBuffer = Util.createFloatBuffer( 16 );
-     Mat4 mapView = Mat4.lookAt( new Triple(50,50,100),
-                                 new Triple(50,50,0),
-                                 new Triple(0,1,0)
-                               );
-System.out.println("mapView:\n " + mapView );
-     mapView.sendData( mapBuffer );
- 
-     // set background color to white
-     GL11.glClearColor( 1.0f, 1.0f, 1.0f, 0.0f );
- 
-     // turn on depth testing
-     GL11.glEnable( GL11.GL_DEPTH_TEST );
-     // clearing the depth buffer means setting all spots to this value (1)
-     GL11.glClearDepth( 2.0f );
-     // an incoming fragment overwrites the existing fragment if its depth
-     // is less
-     GL11.glDepthFunc( GL11.GL_LESS );
- 
-   }
- 
-    protected void processInputs()
+    // read data to build all the Things
+    try 
     {
-       // process all waiting input events
- 
-       while( InputInfo.size() > 0 ) {
-          InputInfo info = InputInfo.get();
- 
-          if( info.kind == 'k' && (info.action == GLFW_PRESS || 
-                                info.action == GLFW_REPEAT) ) {
- 
-             // store info values in more convenient variables
-             int code = info.code;
-             int mods = info.mods;
- 
-             final double amount = 1;  // amount to move
-             final double angAmount = 5;
- 
-             // move the eye point of the camera
-        
-             if ( code == GLFW_KEY_X && mods == 0 ) {// x:  move left
-                camera.shift( -amount, 0, 0 );
-             }
-             else if ( code == GLFW_KEY_X && mods == 1 ) {// X:  move right
-                camera.shift( amount, 0, 0 );
-             }
-             else if ( code == GLFW_KEY_Y && mods == 0 ) {// y:  move smaller in y direction
-                camera.shift( 0, -amount, 0 );
-             }
-             else if ( code == GLFW_KEY_Y && mods == 1 ) {// Y:  move bigger in y direction
- camera.shift( 0, amount, 0 );
-             }
-             else if ( code == GLFW_KEY_Z && mods == 0 ) {// z:  move smaller in z direction
-                camera.shift( 0, 0, -amount );
-             }
-             else if ( code == GLFW_KEY_Z && mods == 1 ) {// Z:  move bigger in Z direction
-                camera.shift( 0, 0, amount );
-             }
-  
-             // change angles
- 
-             else if ( code == GLFW_KEY_R && mods == 0 ) {// r:  rotate clockwise
-                camera.rotate( -angAmount );
-             }
-             else if ( code == GLFW_KEY_R && mods == 1 ) {// R: rotate counterclockwise
-                camera.rotate( angAmount );
-             }
-             else if ( code == GLFW_KEY_T && mods == 0 ) {// t:  tilt downward
-                camera.tilt( -angAmount );
-             }
-             else if ( code == GLFW_KEY_T && mods == 1 ) {// T: tilt upward
-                camera.tilt( angAmount );
-             }
-         
-          }// input event is a key
- 
-          else if ( info.kind == 'm' ) {// mouse moved
-             //  System.out.println( info );
-          }
- 
-          else if( info.kind == 'b' ) {// button action
-             //  System.out.println( info );
-          }
- 
-       }// loop to process all input events
- 
-    }
- 
-   protected void update() {
-      // give each thing a chance to update itself
-      for (int k=0; k<things.size(); k++) {
-System.out.println("update things # " + k + " with id " + things.get(k).getId());
-         things.get(k).update();
+      Scanner input = new Scanner( new File( inputFile ) );
+      int number = input.nextInt();  input.nextLine();
+      things = new ArrayList<Thing>();
+      for (int k=0; k<number; k++) 
+      {
+        things.add( new Thing( input ) );           
       }
+        input.close();
+    }
+    catch(Exception e) 
+    {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    // place camera somewhere at start
+    camera = new Camera( -.1, .1, -.1, .1, 0.5, 300, new Triple(50,50,30), 90, -30 ); 
+  }
+
+  /**
+   * init (overrides Basic.init())
+   */
+  protected void init()
+  {
+    String vertexShaderCode =
+      "#version 330 core\n"+
+      "layout (location = 0 ) in vec3 vertexPosition;\n"+
+      "layout (location = 1 ) in vec2 vertexTexCoord;\n"+
+      "out vec2 texCoord;\n"+
+      "uniform mat4 projection;\n"+
+      "uniform mat4 view;\n"+
+      "void main(void)\n"+
+      "{\n"+
+      "  texCoord = vertexTexCoord;\n"+
+      "  gl_Position = projection * view * vec4( vertexPosition, 1.0 );\n"+
+      "}\n";
+    System.out.println("Vertex shader:\n" + vertexShaderCode + "\n\n" );
+    v1 = new Shader( "vertex", vertexShaderCode );
+    String fragmentShaderCode =
+      "#version 330 core\n"+
+      "in vec2 texCoord;\n"+
+      "layout (location = 0 ) out vec4 fragColor;\n"+
+      "uniform sampler2D texture1;\n"+
+      "void main(void)\n"+
+      "{\n"+
+      "  fragColor = texture( texture1, texCoord );\n"+
+      "}\n";
+    System.out.println("Fragment shader:\n" + fragmentShaderCode + "\n\n" );
+    f1 = new Shader( "fragment", fragmentShaderCode );
+    hp1 = GL20.glCreateProgram();
+    Util.error("after create program");
+    System.out.println("program handle is " + hp1 );
+    GL20.glAttachShader( hp1, v1.getHandle() );
+    Util.error("after attach vertex shader to program");
+    GL20.glAttachShader( hp1, f1.getHandle() );
+    Util.error("after attach fragment shader to program");
+    GL20.glLinkProgram( hp1 );
+    Util.error("after link program" );
+    GL20.glUseProgram( hp1 );
+    Util.error("after use program");
+    // get location of uniforms 
+    projLoc = GL20.glGetUniformLocation( hp1, "projection" );
+    viewLoc = GL20.glGetUniformLocation( hp1, "view" );
+    texture1Loc = GL20.glGetUniformLocation( hp1, "texture1" );
+    // once and for all (it's never changing), get
+    // buffer for frustum
+    frustumBuffer = camera.getFrustumBuffer();
+    // and create the fixed ortho and identity matrix buffers
+    orthoBuffer = Util.createFloatBuffer( 16 );
+    Mat4 ortho = Mat4.ortho( -50, 50, -50, 50, 0, 101 );
+    System.out.println("ortho: \n" + ortho );
+    ortho.sendData( orthoBuffer );
+    mapBuffer = Util.createFloatBuffer( 16 );
+    Mat4 mapView = Mat4.lookAt( new Triple(50,50,100),
+                                new Triple(50,50,0),
+                                new Triple(0,1,0)
+                               );
+    System.out.println("mapView:\n " + mapView );
+    mapView.sendData( mapBuffer );
+    // set background color to white
+    GL11.glClearColor( 1.0f, 1.0f, 1.0f, 0.0f );
+    // turn on depth testing
+    GL11.glEnable( GL11.GL_DEPTH_TEST );
+    // clearing the depth buffer means setting all spots to this value (1)
+    GL11.glClearDepth( 2.0f );
+    // an incoming fragment overwrites the existing fragment if its depth
+    // is less
+    GL11.glDepthFunc( GL11.GL_LESS );
    }
  
-   protected void display() {
- // System.out.println( "Step: " + getStepNumber() + " camera: " + camera );
-
-     // clear the color and depth buffers
-     GL11.glClear( GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT );
-
-     // create tris from the things:
-     tris = new TriList();
-
-     for (int k=0; k<things.size(); k++) {
-         things.get(k).sendTriangles( tris );
-     }
-
-     // ---------------------------------------------------
-     // for each texture, draw all triangles that use it
-
-     for (int k=0; k<Pic.size(); k++ ){
-
-        // send texture sampler as a uniform
-        GL20.glUniform1i( texture1Loc, 0 ); 
-           Util.error("after set value of texture1");
-
-        // set up the texture in GPU
-        Pic.get(k).activate();
-
-        // send triangle data to GPU
-        // and note how many there were
-        int count = tris.sendData( k );
-
-        // activate vao
-        GL30.glBindVertexArray( tris.getVAO() );
-            Util.error("after bind vao");
+  /**
+   * processInputs (overrides Basic.processInputs())
+   */
+  protected void processInputs()
+  {
+    // process all waiting input events
+    while( InputInfo.size() > 0 ) 
+    {
+      InputInfo info = InputInfo.get();
+      if( info.kind == 'k' && (info.action == GLFW_PRESS 
+        || info.action == GLFW_REPEAT) )
+      {
+        // store info values in more convenient variables
+        int code = info.code;
+        int mods = info.mods;
+        final double amount = 1;  // amount to move
+        final double angAmount = 5;
+        // move the eye point of the camera
+        if ( code == GLFW_KEY_X && mods == 0 )
+        { // x:  move left
+          camera.shift( -amount, 0, 0 );
+        }
+        else if ( code == GLFW_KEY_X && mods == 1 )
+        { // X:  move right
+          camera.shift( amount, 0, 0 );
+        }
+        else if ( code == GLFW_KEY_Y && mods == 0 )
+        { // y:  move smaller in y direction
+          camera.shift( 0, -amount, 0 );
+        }
+        else if ( code == GLFW_KEY_Y && mods == 1 )
+        { // Y:  move bigger in y direction
+          camera.shift( 0, amount, 0 );
+        }
+        else if ( code == GLFW_KEY_Z && mods == 0 ) 
+        { // z:  move smaller in z direction
+          camera.shift( 0, 0, -amount );
+        }
+        else if ( code == GLFW_KEY_Z && mods == 1 ) 
+        { // Z:  move bigger in Z direction
+          camera.shift( 0, 0, amount );
+        }
+        // change angles //
+        else if ( code == GLFW_KEY_R && mods == 0 ) 
+        { // r:  rotate clockwise
+          camera.rotate( -angAmount );
+        }
+        else if ( code == GLFW_KEY_R && mods == 1 )
+        { // R: rotate counterclockwise
+          camera.rotate( angAmount );
+        }
+        else if ( code == GLFW_KEY_T && mods == 0 )
+        { // t:  tilt downward
+          camera.tilt( -angAmount );
+        }
+        else if ( code == GLFW_KEY_T && mods == 1 )
+        { // T: tilt upward
+          camera.tilt( angAmount );
+        }
+      } // input event is a key
+      else if ( info.kind == 'm' ) 
+      { // mouse moved
+        //  System.out.println( info );
+      }
+      else if( info.kind == 'b' )
+      { // button action
+        //  System.out.println( info );
+      }
+    } // loop to process all input events
+  }
  
-        //--------------------------------------
-        // draw view of world in left sub-window
-        // using lookAt and frustum
-        //--------------------------------------
-        GL11.glViewport( 0, 0, 
-                         Util.retinaDisplay*getPixelWidth()/2, 
-                         Util.retinaDisplay*getPixelHeight() );
-
-        GL20.glUniformMatrix4fv( projLoc, true, frustumBuffer ); 
-        lookAtBuffer = camera.getLookAtBuffer();
-        GL20.glUniformMatrix4fv( viewLoc, true, lookAtBuffer ); 
+  /**
+   * update (overrides Basic.update())
+   */
+  protected void update()
+  {
+    // give each thing a chance to update itself
+    for (int k=0; k<things.size(); k++)
+    {
+      System.out.println("update things # " + k + " with id " + things.get(k).getId());
+      things.get(k).update();
+    }
+  }
  
-        // draw the buffers
-        GL11.glDrawArrays( GL11.GL_TRIANGLES, 0, count*3 );
-            Util.error("after draw arrays");
-
-        //--------------------------------------
-        // draw view of world in right sub-window
-        // using ortho and identity
-        //--------------------------------------
-        GL11.glViewport( Util.retinaDisplay*getPixelWidth()/2, 0,
-                         Util.retinaDisplay*getPixelWidth()/2,
-                         Util.retinaDisplay*getPixelHeight() );
-
-        GL20.glUniformMatrix4fv( projLoc, true, orthoBuffer );
-        GL20.glUniformMatrix4fv( viewLoc, true, mapBuffer );
-
-        // draw the buffers
-        GL11.glDrawArrays( GL11.GL_TRIANGLES, 0, count*3 );
-            Util.error("after draw arrays");
-
-      }// draw triangles for pic k
- 
-   }// display
- 
-}// Ex9
+  protected void display()
+  {
+    // System.out.println( "Step: " + getStepNumber() + " camera: " + camera );
+    // clear the color and depth buffers
+    GL11.glClear( GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT );
+    // create tris from the things:
+    tris = new TriList();
+    for (int k=0; k<things.size(); k++)
+    {
+      things.get(k).sendTriangles( tris );
+    }
+    // ---------------------------------------------------
+    // for each texture, draw all triangles that use it
+    for (int k=0; k<Pic.size(); k++ )
+    {
+      // send texture sampler as a uniform
+      GL20.glUniform1i( texture1Loc, 0 ); 
+      Util.error("after set value of texture1");
+      // set up the texture in GPU
+      Pic.get(k).activate();
+      // send triangle data to GPU
+      // and note how many there were
+      int count = tris.sendData( k );
+      // activate vao
+      GL30.glBindVertexArray( tris.getVAO() );
+      Util.error("after bind vao");
+      //--------------------------------------
+      // draw view of world in left sub-window
+      // using lookAt and frustum
+      //--------------------------------------
+      GL11.glViewport( 0, 0, 
+                       Util.retinaDisplay*getPixelWidth()/2, 
+                       Util.retinaDisplay*getPixelHeight() );
+      GL20.glUniformMatrix4fv( projLoc, true, frustumBuffer ); 
+      lookAtBuffer = camera.getLookAtBuffer();
+      GL20.glUniformMatrix4fv( viewLoc, true, lookAtBuffer ); 
+      // draw the buffers
+      GL11.glDrawArrays( GL11.GL_TRIANGLES, 0, count*3 );
+      Util.error("after draw arrays");
+      //--------------------------------------
+      // draw view of world in right sub-window
+      // using ortho and identity
+      //--------------------------------------
+      GL11.glViewport( Util.retinaDisplay*getPixelWidth()/2, 0,
+                       Util.retinaDisplay*getPixelWidth()/2,
+                       Util.retinaDisplay*getPixelHeight() );
+      GL20.glUniformMatrix4fv( projLoc, true, orthoBuffer );
+      GL20.glUniformMatrix4fv( viewLoc, true, mapBuffer );
+      // draw the buffers
+      GL11.glDrawArrays( GL11.GL_TRIANGLES, 0, count*3 );
+      Util.error("after draw arrays");
+    } // draw triangles for pic k
+  } // display
+} // Ex9
